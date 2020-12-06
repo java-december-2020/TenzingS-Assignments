a40019e9f107b3afe5bb6efd293a6c7265d14951
Tenzing Shema <tenzingshema@gmail.com>
2020-12-05 20:48:32 -0600

    my first program

---
+++ 01-java-fundamentals/01-first-java-program/MyFirstProgram.java
@@ -0,0 +1,7 @@
+public class MyFirstProgram {
+    public static void main(String[] args) {
+        System.out.println("My Name is Tenzing Shema");
+        System.out.println("I am 29 years old");
+        System.out.println("My hometown is Chicago, IL");
+    }
+}
