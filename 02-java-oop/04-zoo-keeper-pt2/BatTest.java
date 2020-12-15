a277344b4acb898bbdf63ef60099fa5318588987
Tenzing Shema <tenzingshema@gmail.com>
2020-12-14 23:10:52 -0600

    Zoo part 2 assignment

---
+++ 02-java-oop/04-zoo-keeper-pt2/BatTest.java
@@ -0,0 +1,17 @@
+public class BatTest{
+
+    public static void main(String[] args){
+
+        Bat batMan = new Bat();
+        
+       batMan.attackTown();
+       batMan.attackTown();
+       batMan.attackTown();
+       batMan.eatHumans();
+       batMan.eatHumans();
+       batMan.fly();
+       batMan.fly();
+       batMan.displayEnergy();
+
+    }
+}
---
+++ 02-java-oop/04-zoo-keeper-pt2/Mammal.java
@@ -0,0 +1,14 @@
+public class Mammal{
+    
+    public int energyLevel = 100; 
+
+    public int displayEnergy(){
+        System.out.println("Energy level: " + energyLevel);
+        return energyLevel; 
+    }
+
+    public Mammal(int energyLevel){
+        this.energyLevel = energyLevel; 
+
+    }
+}
