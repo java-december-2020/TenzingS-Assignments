bf1d1ae7b3f1e00fa00af5219295ce62eabc3775
Tenzing Shema <tenzingshema@gmail.com>
2020-12-15 01:35:44 -0600

    bank account assignments

---
+++ 02-java-oop/07-bank-account/BankAccount.java
@@ -0,0 +1,81 @@
+import java.util.Random;
+
+public class BankAccount {
+
+	public static int numAccounts = 0;
+	public static double totalHoldings = 0;
+	private static String generateId() {
+		String subst = "";
+		Random rd = new Random();
+		for(int i = 0; i < 10; i++) {
+			subst += rd.nextInt(10);
+		}
+		return subst;
+	}
+
+	private String accountNumber;
+	private double checkingBalance;
+	private double savingsBalance;
+
+	public BankAccount() {
+		BankAccount.numAccounts += 1;
+		this.checkingBalance = 0;
+		this.savingsBalance = 0;
+		this.accountNumber = BankAccount.generateId();
+	}
+	
+	public double getCheckingBalance() {
+		return this.checkingBalance;
+	}
+
+	public double getSavingsBalance() {
+		return this.savingsBalance;
+	}
+
+	public void depoMoney(double amount, String account) {
+		if(account.equals("savings"))
+			this.savingsBalance += amount;
+		else if(account.equals("checking"))
+			this.checkingBalance += amount;
+		BankAccount.totalHoldings += amount;
+	}
+
+	public void withdrawMoney(double amount, String account) {
+		boolean successful = false;
+		if(account.equals("savings")) {
+			if(this.savingsBalance - amount >= 0) {
+				successful = true;
+				this.savingsBalance -= amount;
+			}
+		}
+		else if(account.equals("checking")) {
+		
+			if(this.checkingBalance - amount >= 0) {
+				successful = true;
+				this.checkingBalance -= amount;
+			}
+		}
+		if(successful) {
+			BankAccount.totalHoldings -= amount;
+		}
+	}
+
+	public void displayActBalance() {
+		System.out.println(String.format("Savings: %.2f, Checking: %.2f", this.savingsBalance, this.checkingBalance));
+	}
+
+	public static void main(String[] args) {
+		BankAccount ba = new BankAccount();
+		ba.depoMoney(100.49, "checking");
+		ba.depoMoney(3.00, "savings");
+		ba.displayActBalance();
+
+		System.out.println(BankAccount.totalHoldings);
+
+		ba.withdrawMoney(50, "checking");
+		ba.withdrawMoney(50, "savings");
+		ba.displayActBalance();
+
+		System.out.println(BankAccount.totalHoldings);
+	}
+}
